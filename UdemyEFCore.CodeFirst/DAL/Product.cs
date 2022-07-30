9956fcb85fa56962a5275595f0c6e57942a37340
celalettin-ceylan <ceylan.celalettin@gmail.com>
2022-07-30 15:57:23 +0300

    Ders 13: add-migration ve update-database

---
+++ UdemyEFCore.CodeFirst/DAL/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyEFCore.CodeFirst.DAL
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int Barcode { get; set; }
+    }
+}
