3773eb9c49d3cd8c1750fa4732390e597b4282ac
celalettin-ceylan <ceylan.celalettin@gmail.com>
2022-07-30 17:19:26 +0300

    Ders 14: remove-migration and script-migration

--- UdemyEFCore.CodeFirst/DAL/Product.cs
+++ UdemyEFCore.CodeFirst/DAL/Product.cs
@@ -5,15 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UdemyEFCore.CodeFirst.DAL
+namespace UdemyEFCore.CodeFirst.DAL;
+
+// once back migration yap update-database yaparak sonra remove-migrationlari silin. aradan silmek yok sondan sile sile gidiyorsun.
+// script-migration ile database olusturdugumuz scriptleri olusturur. olusturulan scripleri sqlserverde calistirilip degisiklikleri direk uygulayabilirsiniz.
+
+public class Product
 {
-    public class Product
-    {
-        [Key]
-        public int Id { get; set; }
-        public String Name { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public int Barcode { get; set; }
-    }
+    [Key]
+    public int Id { get; set; }
+    public String Name { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public int Barcode { get; set; }
 }
