35eeae49e9cf0cbcde2ad1c56ca2ee1d006d8cb6
celalettin-ceylan <ceylan.celalettin@gmail.com>
2022-07-30 08:33:50 +0300

    Add project files.

---
+++ UdemyEFCore.DatabaseFirst/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+using UdemyEFCore.DatabaseFirst.DAL;
+
+using (var _context = new AppDbContext()) {
+
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine("Id: {0}, Name: {1}", p.Id, p.Name);
+    });
+}
