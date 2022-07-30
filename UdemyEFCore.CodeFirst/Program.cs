2767e62ab68720d2aa7c6d69b5d2b26bab01732b
celalettin-ceylan <ceylan.celalettin@gmail.com>
2022-07-31 00:12:27 +0300

    Ders 16 DbContext States (Added, Deleted, Detaced, Modified, Unchanged)

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -5,11 +5,27 @@ using UdemyEFCore.CodeFirst.DAL;
 
 Console.WriteLine("Hello, World!");
 
+// Unchanged : ilk cekilen dataya state olarak atar. data database ile ayni. 
+
 using (var context = new AppDbContext())
 {
+    var newProduct = new Product { Name = "Kalem 200", Price = 200, Stock = 100, Barcode = 115 };
+    // Detached Stated : Not traking state in memory.
+    Console.WriteLine($"First State With Creation: {context.Entry(newProduct).State}");
+    await context.AddAsync(newProduct); // ==> context.Entry(newProduct).State = EntryState.Added;
+    // Added Stated : traking state in memory and state is "added".
+    Console.WriteLine($"Second State With Add Function: {context.Entry(newProduct).State}");
+
+    await context.SaveChangesAsync();
+    // Unchanged Stated : traking state in memory and state is "added".
+    Console.WriteLine($"State After Save Changes : {context.Entry(newProduct).State}");
+
     var products = await context.Products.ToListAsync();
-    products.ForEach(p =>
+    products.ForEach (p =>
     {
-        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock}");
-    });
+        var state = context.Entry(p).State;
+        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock} , state: {state}");
+    }) ;
+
+    // Update metodu id verildigi zaman EF traker etmese bile direk isini yapar.
 }
